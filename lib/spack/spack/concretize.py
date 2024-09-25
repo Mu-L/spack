426caf0b934382087ba27f94e6d61208cf153e97
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2024-09-25 19:40:03 +0200

    Overhaul of the spack.compilers package
    Now the package contains modules that help using, or
    detecting, compiler packages.
    
    Signed-off-by: Massimiliano Culpo <massimiliano.culpo@gmail.com>

--- lib/spack/spack/concretize.py
+++ lib/spack/spack/concretize.py
@@ -146,7 +146,7 @@ def concretize_separately(
 
     # Ensure we have compilers in compilers.yaml to avoid that
     # processes try to write the config file in parallel
-    _ = spack.compilers.all_compilers_config(spack.config.CONFIG)
+    _ = spack.compilers.config.all_compilers_from(spack.config.CONFIG)
 
     # Early return if there is nothing to do
     if len(args) == 0:
